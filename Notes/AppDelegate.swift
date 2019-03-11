7d0d144c8fc763b12ffe2bbc1083ff131db1fb67
Vadim Koronchik <koronchik@icloud.com>
2019-03-11 20:02:07 +0300

    Initial Commit

---
+++ Notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Vadim Koronchik on 3/11/19.
+//  Copyright © 2019 Vadim Koronchik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
